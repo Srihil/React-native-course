9c63d5d8e1077b2be6b05fb8c29880d2710fffcb
Sahil <n.sahil2709@gmail.com>
2026-03-26 11:21:04 +0530

    implemented Scroll view for scrolling

--- React_native_modules/CoreComponents/App.js
+++ React_native_modules/CoreComponents/App.js
@@ -1,14 +1,30 @@
-import { View, Text,Image, ImageBackground } from "react-native";
+import { View, Text,Image, ImageBackground, ScrollView } from "react-native";
 const logo = require("./assets/adaptive-icon.png")
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      {/* <Image source={logo} style={{ height: 300, width: 300}}/>
-      <Image source={{uri : "https://picsum.photos/300"}} style={{ height: 300, width: 300}}/> */}
-      <ImageBackground source={logo} style={{ flex: 1 }}>
-        <Text>Image Text</Text>
-      </ImageBackground>
+      <ScrollView>
+        <Image source={logo} style={{ width: 300, height: 300 }} />
+        
+        <Text>
+          lorem You can generate Lorem Ipsum in VS Code using the built-in Emmet abbreviation support or by installing an extension. 
+          YouTube
+          YouTube
+          +2
+          Using Built-in Emmet (Recommended)
+          VS Code includes built-in Emmet support, so you don't need any extra extensions for basic generation. 
+          Generate a default paragraph: Type lorem and then press the Tab key or Enter.
+          Generate a specific number of words: Type lorem followed by the number of words you need (e.g., lorem20 for 20 words), then press Tab or Enter.
+          Generate within HTML tags: Combine lorem with other Emmet abbreviations.
+          To create a paragraph element with 50 words, type plorem50 and press Tab.
+          To create three paragraphs, each with 5 words, type p*3lorem5 and press Tab. 
+          YouTube
+          YouTube
+          +4
+        </Text>
+        <Image source={logo} style={{ width: 300, height: 300 }} />
+      </ScrollView>
     </View>
   );
 }
