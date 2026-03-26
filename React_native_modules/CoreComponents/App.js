6dd5c400e2a06cf6af8848b1ebdc0498e1f7535b
Sahil <n.sahil2709@gmail.com>
2026-03-26 12:39:22 +0530

    implemented Custom component and imported in the app.js

--- React_native_modules/CoreComponents/App.js
+++ React_native_modules/CoreComponents/App.js
@@ -3,27 +3,13 @@ import {
   Alert,
   Button,
 } from "react-native";
-import { useState } from "react";
-
-const logo = require("./assets/adaptive-icon.png");
+import Greet from "./components/Greet";
 
 export default function App() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Button title="alert" onPress={() =>Alert.alert("Invalid Data!")}/>
-      <Button title="alert 2" onPress={() =>Alert.alert("Invalid Data!", "DOB incorrect")}/>
-      <Button title="alert 3" onPress={() =>Alert.alert("Invalid Data!", "DOB incorrect", [
-        {
-          text: "Cancel",
-          onPress: () => console.log("cancel pressed") ,
-        },
-        {
-          text: "Ok",
-          onPress: () => console.log("Ok pressed"),
-        },
-        
-      ])}/>
+      <Greet name="Sahil"/>
+      <Greet name="Batman"/>
     </View>
   );
 }
