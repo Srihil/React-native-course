cd84270e21bcabe82f342de592285c686414e74d
Sahil <n.sahil2709@gmail.com>
2026-03-26 12:14:55 +0530

    implemented Modal component

--- React_native_modules/CoreComponents/App.js
+++ React_native_modules/CoreComponents/App.js
@@ -8,38 +8,34 @@ import {
   Button,
   Modal,
 } from "react-native";
+import { useState } from "react";
+
 const logo = require("./assets/adaptive-icon.png");
 
 export default function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <ScrollView>
-        <Button
-          title="press"
-          onPress={() => console.log("Button pressed")}
-          color="midnightblue"
-        />
-        <Pressable onPress={() => console.log("Image pressed")}>
-          <Image source={logo} style={{ width: 300, height: 300 }} />
-        </Pressable>
-        <Pressable onPress={() => console.log("Image pressed")}>
-          <Text>
-            lorem You can generate Lorem Ipsum in VS Code using the built-in
-            Emmet abbreviation support or by installing an extension. YouTube
-            YouTube +2 Using Built-in Emmet (Recommended) VS Code includes
-            built-in Emmet support, so you don't need any extra extensions for
-            basic generation. Generate a default paragraph: Type lorem and then
-            press the Tab key or Enter. Generate a specific number of words:
-            Type lorem followed by the number of words you need (e.g., lorem20
-            for 20 words), then press Tab or Enter. Generate within HTML tags:
-            Combine lorem with other Emmet abbreviations. To create a paragraph
-            element with 50 words, type plorem50 and press Tab. To create three
-            paragraphs, each with 5 words, type p*3lorem5 and press Tab. YouTube
-            YouTube +4
-          </Text>
-        </Pressable>
-        <Image source={logo} style={{ width: 300, height: 300 }} />
-      </ScrollView>
+      <Button
+        title="press"
+        onPress={() => setIsModalVisible(true)}
+        color="midnightblue"
+      />
+      <Modal
+        visible={isModalVisible}
+        onRequestClose={() => setIsModalVisible(false)}
+        animationType="slide"
+        presentationStyle="formSheet" //only on IOS
+      >
+        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
+          <Text>Modal content</Text>
+          <Button
+            title="close"
+            color="midnightblue"
+            onPress={() => setIsModalVisible(false)}
+          />
+        </View>
+      </Modal>
     </View>
   );
 }
