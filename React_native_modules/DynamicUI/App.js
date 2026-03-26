d7305ac980c9cf20350a87a3d8ccdbc88c63f3c0
Sahil <n.sahil2709@gmail.com>
2026-03-26 17:49:26 +0530

    created an landscape dynamic layout for any screen size

--- React_native_modules/DynamicUI/App.js
+++ React_native_modules/DynamicUI/App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function App() {
+  const[dimensions, setDimensions] = useState({
+    window: Dimensions.get("window"),
+  })
+
+  useEffect(() =>{
+    const subscription = Dimensions.addEventListener("change", ({ window }) => {setDimensions({ window });});
+    return () => subscription?.remove();
+  })
+
+  const { window } = dimensions;
+  const windowwidth = window.width;
+  const windowheight = window.height;
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={[styles.box,
+        {
+          width: windowwidth > 500 ? "70%" : "90%",
+          height: windowheight > 600 ? "60%" : "90%",
+        }
+      ]}>
+        <Text style={{fontSize: windowwidth > 500 ? 50 : 24}}>Welcome!</Text>
+      </View>
     </View>
   );
 }
 
+const windowwidth = Dimensions.get("window").width
+const windowheight = Dimensions.get("window").height
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'plum',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  box: {
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  // text: {
+    
+
+  // },
 });
