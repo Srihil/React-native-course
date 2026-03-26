df1e89eedba5c27097170af6aa52e1d6e755c5f7
Sahil <n.sahil2709@gmail.com>
2026-03-26 12:52:42 +0530

    implemented StyleSheet and how to apply mutltiple styles to a component

--- React_native_modules/CoreComponents/App.js
+++ React_native_modules/CoreComponents/App.js
@@ -1,15 +1,33 @@
 import {
   View,
-  Alert,
-  Button,
+  Text,
+  StyleSheet,
 } from "react-native";
-import Greet from "./components/Greet";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Greet name="Sahil"/>
-      <Greet name="Batman"/>
+    <View style={styles.container}>
+      <View style={[styles.Box, styles.lightblueBg]}>
+        <Text>LightBlue box</Text>
+      </View>
+      <View  style={[styles.Box, styles.lightgreenBg]}>
+        <Text>LightGreen box</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "plum", padding: 60 },
+  Box: {
+    width: 100,
+    height: 100,
+    padding: 10,
+  },
+  lightblueBg: {
+    backgroundColor: "lightblue",
+  },
+  lightgreenBg: {
+    backgroundColor: "lightgreen",
+  }
+})
