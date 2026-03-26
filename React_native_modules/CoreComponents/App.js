fdcf233661eaf6959b4bd024693abb97334baea7
Sahil <n.sahil2709@gmail.com>
2026-03-26 11:13:10 +0530

    implemented stylings as Views, Text, Image, ImageBackground

--- React_native_modules/CoreComponents/App.js
+++ React_native_modules/CoreComponents/App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text,Image, ImageBackground } from "react-native";
+const logo = require("./assets/adaptive-icon.png")
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      {/* <Image source={logo} style={{ height: 300, width: 300}}/>
+      <Image source={{uri : "https://picsum.photos/300"}} style={{ height: 300, width: 300}}/> */}
+      <ImageBackground source={logo} style={{ flex: 1 }}>
+        <Text>Image Text</Text>
+      </ImageBackground>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
