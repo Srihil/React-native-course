6aff4edafaf08a711534762afc2e4392a1cf2013
Sahil <n.sahil2709@gmail.com>
2026-03-26 10:38:32 +0530

    created a expo app called Corecomponents to learn stylings in phone devices using react-native

--- React_native_modules/HelloWorld/app/(tabs)/index.tsx
+++ React_native_modules/HelloWorld/app/(tabs)/index.tsx
@@ -18,11 +18,11 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Welcome</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle">Ste</ThemedText>
         <ThemedText>
           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
           Press{' '}
@@ -39,7 +39,7 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <Link href="/modal">
           <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+            <ThemedText type="subtitle">Step 2: Sahil</ThemedText>
           </Link.Trigger>
           <Link.Preview />
           <Link.Menu>
